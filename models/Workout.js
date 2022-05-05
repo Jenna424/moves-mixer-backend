0d9bc808aa321b217e491f4541088b39a7c78edb
Jenna Leopold <jleopold424@gmail.com>
2022-05-05 10:29:06 -0400

    add trainingType to Workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -2,6 +2,7 @@ const { Schema } = require('mongoose')
 const Workout = new Schema(
   {
     title: { type: String, required: true },
+    trainingType: { type: String, required: true },
     duration: { type: String, required: true },
     image: { type: String, required: true },
     exercises: [
