c8276718d5d8dc9f850e676f97db2b11c870db6b
Jenna Leopold <jleopold424@gmail.com>
2022-05-05 10:15:04 -0400

    add image to Workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -3,6 +3,7 @@ const Workout = new Schema(
   {
     title: { type: String, required: true },
     duration: { type: String, required: true },
+    image: { type: String, required: true },
     exercises: [
       { type: Schema.Types.ObjectId, ref: 'Exercise', required: true }
     ],
