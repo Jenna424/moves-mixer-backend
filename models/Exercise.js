79fb5fe4021901d5ed7dcfd60a2700f13a7f4257
Jenna Leopold <jleopold424@gmail.com>
2022-05-05 10:16:14 -0400

    created Exercise schema

---
+++ models/Exercise.js
@@ -0,0 +1,13 @@
+const { Schema } = require('mongoose')
+const Exercise = new Schema(
+  {
+    name: { type: String, required: true },
+    instructions: { type: String, required: true },
+    sets: { type: Number, required: true },
+    reps: { type: Number, required: true },
+    image: { type: String, required: true },
+    workout: { type: Schema.Types.ObjectId, ref: 'Workout', required: true }
+  },
+  { timestamps: true }
+)
+module.exports = Exercise
