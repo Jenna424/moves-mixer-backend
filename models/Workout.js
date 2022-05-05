0ccc91f616721b0d0b57954937ff908ed9f53ced
Jenna Leopold <jleopold424@gmail.com>
2022-05-05 10:51:29 -0400

    change duration to Number type in Workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,7 @@ const Workout = new Schema(
   {
     title: { type: String, required: true },
     trainingType: { type: String, required: true },
-    duration: { type: String, required: true },
+    duration: { type: Number, required: true },
     image: { type: String, required: true },
     exercises: [
       { type: Schema.Types.ObjectId, ref: 'Exercise', required: true }
