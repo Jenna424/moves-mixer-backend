2e90bb13ca72e75a2b0e6c26ec9df1a3272a6f2a
Jenna Leopold <jleopold424@gmail.com>
2022-05-09 10:21:55 -0400

    add completed field to Exercise schema

--- models/Exercise.js
+++ models/Exercise.js
@@ -6,6 +6,10 @@ const Exercise = new Schema(
     sets: { type: Number, required: true },
     reps: { type: Number, required: true },
     image: { type: String, required: true },
+    completed: {
+      type: Boolean,
+      default: false
+    },
     workout: { type: Schema.Types.ObjectId, ref: 'Workout', required: true }
   },
   { timestamps: true }
