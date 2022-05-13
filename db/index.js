c4ccda6a2094090c658442bcad6e6b6efca44049
Jenna Leopold <jleopold424@gmail.com>
2022-05-13 12:11:52 -0400

    heroku app BE

--- db/index.js
+++ db/index.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/workoutRoutinesDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/workoutRoutinesDatabase'
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
